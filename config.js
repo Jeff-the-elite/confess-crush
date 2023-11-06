d341a14d63e657d940d64144de0a1d485b6fdbc9
Jeff-the-elite <150056211+Jeff-the-elite@users.noreply.github.com>
2023-11-06 22:35:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "Hey Handsome~😘",
+    introTitle: 'je t’aime chéri chignon',
+    introDesc: `Your lips are like the petals of a lush orchid, their velvety softness tempting me to taste their exotic allure.`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    title: 'If I could command the clouds, I’d create a sky of cotton candy, a sweet confection that mirrors the tenderness of your touch.',
+    desc: 'Do you love me, my Darling Bel  ',
+    btnYes: 'FUCK YEAH😘',
+    btnNo: 'HELL NO (u r gay🥰):3',
+    question: 'Why are you Soo damn handsome abel ohhh myyy gooodddd😩😩',
+    btnReply: 'IS that ur reply bella??touch here cutie<3',
+    reply: 'Thanks, but I have a Girlfriend<33333333',
+    mess: 'I know bae🥰.Its me,aint it?! Honey bunny sugerpie😘❤️',
+    messDesc: 'Your eyes sparkle like the finest champagne, intoxicating me with their effervescent allure❤️.Good night❤️❤️',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://www.instagram.com/jeff_the_elite/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
